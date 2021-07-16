51dbc4ed2444a5426ccd99b88af67f434e19e8a6
Thiago Santiago <thiagosantiagoss20@gmail.com>
2021-07-16 14:44:00 -0300

    Feat: Initial struct

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Chat from './Components/Chat';
 
 class App extends React.Component {
   render() {
     return (
       <main className="App">
-        Components
+        <Chat />
       </main>
     );
   }
---
+++ whatslab/src/Components/Chat/style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const ChatWrapper = styled.section`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  
+  div {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    background: #e5ddd5;
+    border: 1px solid black;
+    height: 100vh;
+    width: 600px;
+    padding: 8px 2%;
+  }
+`;
