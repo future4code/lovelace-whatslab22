5b8b8c0ce72953c375c0f2996c6866b9e322d4e5
Thiago Santiago <thiagosantiagoss20@gmail.com>
2021-07-16 13:10:39 -0300

    Feat: Inital Whatslab project

---
+++ whatslab/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main className="App">
+        Components
+      </main>
+    );
+  }
+}
+
+export default App;
