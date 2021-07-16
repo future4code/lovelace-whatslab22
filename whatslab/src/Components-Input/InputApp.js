8637e09cb7d63936248cb88c9a304232297e1dc4
camilagsoares <camila.soares19@hotmail.com>
2021-07-16 15:48:14 -0300

    Posicionei os campos de input

--- whatslab/src/Components-Input/InputApp.js
+++ whatslab/src/Components-Input/InputApp.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
+
+
 class Input extends React.Component {
+  
     render() {
      
       return (
-        <div>
+      
           <input
           
             placeholder={this.props.placeholder}
           />
-        </div>
+        
+       
       );
     }
   }
--- whatslab/src/Components/Chat/index.js
+++ whatslab/src/Components/Chat/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { ChatWrapper } from './style';
+import Input from '../../Components-Input/InputApp'
 
 class Chat extends React.Component {
   render() {
     return (
       <ChatWrapper>
-        <div></div>
+      
+        <div >
+        <section>
+          Texto
+         
+          </section>
+          <form>
+          <Input 
+          placeholder = "Usuário"
+          /> 
+          <Input 
+          placeholder = "Mensagem"
+          /> 
+          <button>Enviar</button>
+          </form>
+        </div>
+       
       </ChatWrapper>
     );
   }
